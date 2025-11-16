53ac7a9b02c15ab1e3739809865d7261132339ec
Man Kumar Tamang <nissanlama2020@gmail.com>
2025-11-16 15:35:16 +0545

    updated Prediction Pipeline

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -2,32 +2,38 @@ import sys
 import pandas as pd
 from src.exception import CustomException
 from src.utils import load_object
-
+import os
 
 class PredictPipeline:
     def __init__(self):
-        pass 
+        pass
 
     def predict(self,features):
-        model_path='artifacts\model.pkl'
-        preprocessor_path='artifacts\preprocessor.pkl'
-        model=load_object(file_path=model_path)
-        preprocessor=load_object(file_path=preprocessor_path)
-        data_scaled=preprocessor.transform(features)
-        preds=model.predict(data_scaled)
-        return preds
+        try:
+            model_path=os.path.join("artifacts","model.pkl")
+            preprocessor_path=os.path.join("artifacts","proprocessor.pkl")
+            print("Before Loading")
+            model=load_object(file_path=model_path)
+            preprocessor=load_object(file_path=preprocessor_path)
+            print("After Loading")
+            data_scaled=preprocessor.transform(features)
+            preds=model.predict(data_scaled)
+            return preds
+        
+        except Exception as e:
+            raise CustomException(e,sys)
 
 
 
 class CustomData:
-    def __init__( self,
-        gender:str,
-        race_ethnicity:str,
+    def __init__(  self,
+        gender: str,
+        race_ethnicity: str,
         parental_level_of_education,
-        lunch:str,
-        test_preparation_course:str,
-        reading_score:int,
-        writing_score:int):
+        lunch: str,
+        test_preparation_course: str,
+        reading_score: int,
+        writing_score: int):
 
         self.gender = gender
 
@@ -56,16 +62,6 @@ class CustomData:
             }
 
             return pd.DataFrame(custom_data_input_dict)
-        
+
         except Exception as e:
-            raise CustomException(e,sys)
-    
-
-
-
-
-
-
-    
-        
-            
+            raise CustomException(e, sys)
